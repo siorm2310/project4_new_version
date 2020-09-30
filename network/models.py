13678bce0ca1a5242264fe398d6159f7d28b64a7
סיאור מאיר <siorm2310@gmail.com>
2020-09-30 15:49:11 +0300

    added Post model, registered models to admin page

--- network/models.py
+++ network/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class User(AbstractUser):
+    # follows = models.ForeignKey("self",on_delete=CASCADE)
     pass
+
+class Post(models.Model):
+    creator = models.ForeignKey(User,on_delete=CASCADE)
+    title = models.CharField(max_length=32)
+    content = models.CharField(max_length=180)
+    likes = models.IntegerField(default=0)
